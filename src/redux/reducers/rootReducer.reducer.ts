ac5803fbc6533b1a57fd11ec2693a02cd3932200
thatipradeepreddy <pradeep.thati@qylis.com>
2024-06-06 11:48:34 +0530

    initial setup of project

---
+++ src/redux/reducers/rootReducer.reducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+const rooReducer = combineReducers({
+
+})
+
+export default rooReducer;
---
+++ src/redux/store.ts
@@ -0,0 +1,5 @@
+import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from 'redux-saga';
+import {all} from "redux-saga/effects";
+
+
