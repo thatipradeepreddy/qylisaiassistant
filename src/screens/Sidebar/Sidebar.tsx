e2c3fa9239594afebf94e550ed0da0f13ede56dd
Amarnath54 <amarnath.bodavula@qylis.com>
2024-06-25 12:26:00 +0530

    UI changes

--- src/screens/Sidebar/Sidebar.tsx
+++ src/screens/Sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Sidebar.css'; // Assuming you have a CSS file for styling
+// import './Sidebar.css'; // Assuming you have a CSS file for styling
 
 interface SidebarProps {
   isOpen: boolean;
